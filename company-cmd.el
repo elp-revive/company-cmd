1fd387205b1821b77149d798ceba76aadfe03963
Jen-Chieh Shen <jcs090218@gmail.com>
2022-07-03 23:46:05 +0800

    src

--- company-cmd.el
+++ company-cmd.el
@@ -3,8 +3,9 @@
 ;; This is free and unencumbered software released into the public domain.
 
 ;; Author: Noah Peart
-;; URL: https://github.com/nverno/company-cmd
-;; Package-Requires ((company "0.8.0") (cl-lib "0.5.0"))
+;; Maintainer: Jen-Chieh Shen <jcs090218@gmail.com>
+;; URL: https://github.com/elp-revive/company-cmd
+;; Package-Requires: ((emacs "26.1") (company "0.8.12") (ht "2.4"))
 ;; Keywords: convenience
 
 ;; This file is not part of GNU Emacs
@@ -36,8 +37,10 @@
 ;; completion-at-point (not provided in base `bat-mode').
 
 ;;; Code:
+
 (eval-when-compile (require 'cl-lib))
 (require 'company)
+(require 'ht)
 
 (defgroup company-cmd nil
   "CMD/DOS completion backend."
@@ -48,19 +51,23 @@
   "Various modes for editing windows batch files."
   :type '(repeat function))
 
-(defvar company-cmd-candidates ()
+(defvar company-cmd--candidates ()
   "List of completion candidates and meta info.")
 
-(defun company-cmd-candidates ()
+(defvar company-cmd--doc-buffers (ht-create)
+  "Store doc-buffer cache.")
+
+(defun company-cmd--candidates ()
   "Build/return list of commands to offer for completion."
-  (or company-cmd-candidates
+  (or company-cmd--candidates
       (let ((raw
              (split-string
               (replace-regexp-in-string
                "\n\\s-+" " "
                (shell-command-to-string
-                "help | findstr /b /v /c:\"For more\"")) "\n" t)))
-        (setq company-cmd-candidates
+                "help | findstr /b /v /c:\"For more\""))
+              "\n" t)))
+        (setq company-cmd--candidates
               (mapcar #'(lambda (str)
                           (let ((pos (string-match "\\s-" str)))
                             (cons (substring str 0 pos)
@@ -69,29 +76,33 @@
                                    (substring str (1+ pos) (length str))))))
                       raw)))))
 
-(defun company-cmd-prefix ()
+(defun company-cmd--prefix ()
   "Prefix for bat completion."
   (and (memq major-mode company-cmd-modes)
        (not (company-in-string-or-comment))
        (company-grab-symbol)))
 
-(defun company-cmd-meta (candidate)
+(defun company-cmd--meta (candidate)
   "Return short documentation string for CANDIDATE."
-  (cdr (assoc-string candidate company-cmd-candidates t)))
+  (cdr (assoc-string candidate company-cmd--candidates t)))
 
-(defun company-cmd-doc (candidate)
+(defun company-cmd--doc (candidate)
   "Return buffer with detailed help for CANDIDATE."
-  (with-temp-buffer
-    (call-process "cmd.exe" nil t nil "/c" (concat " help " candidate))
-    (company-doc-buffer (buffer-string))))
+  (company-doc-buffer
+   (or (ht-get company-cmd--doc-buffers candidate)
+       (let ((doc (with-temp-buffer
+                    (call-process "cmd.exe" nil t nil "/c" (concat " help " candidate))
+                    (buffer-string))))
+         (ht-set company-cmd--doc-buffers candidate doc)
+         doc))))
 
 (defun company-cmd-offer-candidates (arg)
   "Offer completion candidates, if an uppercase character is found,
 the candidates are all uppercase."
   (let* ((case-fold-search nil)
-        (to-upper (string-match-p "[A-Z]+" arg))
-        (completion-ignore-case t)
-        (res (all-completions arg (company-cmd-candidates))))
+         (to-upper (string-match-p "[A-Z]+" arg))
+         (completion-ignore-case t)
+         (res (all-completions arg (company-cmd--candidates))))
     (if to-upper res
       (mapcar #'downcase res))))
 
@@ -101,11 +112,10 @@ the candidates are all uppercase."
   (interactive (list 'interactive))
   (cl-case command
     (interactive (company-begin-backend 'company-cmd))
-    (prefix (company-cmd-prefix))
-    (meta (company-cmd-meta arg))
+    (prefix (company-cmd--prefix))
+    (meta (company-cmd--meta arg))
     (candidates (company-cmd-offer-candidates arg))
-    (doc-buffer (company-cmd-doc arg))))
+    (doc-buffer (company-cmd--doc arg))))
 
 (provide 'company-cmd)
-
 ;;; company-cmd.el ends here
